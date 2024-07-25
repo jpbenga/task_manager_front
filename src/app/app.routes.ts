67444e3fabb936e73259696d0058a759e8e0b29a
jpbenga <jpaulbenga@gmail.com>
2024-07-26 00:26:12 +0200

    mise en place des composants

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'task-detail', component: TaskDetailComponent},
+    {path: 'task-form', component: TaskFormComponent},
+    {path: 'task-list', component: TaskListComponent},
+];
