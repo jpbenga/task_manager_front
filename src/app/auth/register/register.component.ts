56189f6cd879a5d4c065af145487ddbfcce4d895
jpbenga <jpaulbenga@gmail.com>
2024-08-10 01:02:02 +0200

    overall apparence fixed

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../user-registration/services/UserService';
@@ -11,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
   selector: 'app-user-registration',
   standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
